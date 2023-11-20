267e3e147c0b7e8b35580134c31066a86a3da2a2
Jack Star <nyanheinaung@gmail.com>
2023-11-20 23:35:49 +0900

    4x TicTacToe 2nd Commit

---
+++ TicTacToe/Assets/Scripts/SGameRules.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SGameRules : MonoBehaviour
+{
+    public Image[] boxes;
+    public bool playerTurn = true;
+    private int totalMoves;
+    public bool gameOver;
+   // private bool winnerDecided;
+
+    // Start is called before the first frame update
+    void Start()
+    {       
+        foreach(Image box in boxes)
+        {
+            box.GetComponentInChildren<Text>().text = "";
+        }
+        totalMoves = 0;
+        gameOver = false;
+    }
+
+    public void CheckCondition()
+    {
+        //Check if there is winner, then current player is the winner
+        if(Winner())
+        {
+            gameOver = true;
+            DisplayWinner();
+        }
+        //If all grids filled and no winner, declare Tie
+        else if(totalMoves == 9)
+        {
+            DisplayTie();
+        }
+        //If no winner, continue to next turn.
+        else
+        {
+            ChangePlayer();
+        }
+    }
+
+    public void ChangePlayer()
+    {
+        playerTurn = !playerTurn;
+    }
+
+    void DisplayWinner()
+    {
+        print("GameOver");
+        
+        if(playerTurn)
+        {  
+            print("Player 1 Wins");
+        }
+        else
+        {
+            print("Player 2 Wins");
+        }
+    }
+
+    void DisplayTie()
+    {
+        print("Tie");
+    }
+
+    public void OneMovePlayed()
+    {
+        totalMoves++;
+    }
+
+    bool Winner()
+    {
+        if(boxes[0].GetComponentInChildren<Text>().text == boxes[1].GetComponentInChildren<Text>().text && boxes[1].GetComponentInChildren<Text>().text  == boxes[2].GetComponentInChildren<Text>().text && boxes[2].GetComponentInChildren<Text>().text != ""||
+            boxes[3].GetComponentInChildren<Text>().text == boxes[4].GetComponentInChildren<Text>().text && boxes[4].GetComponentInChildren<Text>().text == boxes[5].GetComponentInChildren<Text>().text && boxes[5].GetComponentInChildren<Text>().text != ""||
+            boxes[6].GetComponentInChildren<Text>().text == boxes[7].GetComponentInChildren<Text>().text && boxes[7].GetComponentInChildren<Text>().text == boxes[8].GetComponentInChildren<Text>().text && boxes[8].GetComponentInChildren<Text>().text != ""||
+            boxes[0].GetComponentInChildren<Text>().text == boxes[3].GetComponentInChildren<Text>().text && boxes[3].GetComponentInChildren<Text>().text == boxes[6].GetComponentInChildren<Text>().text && boxes[6].GetComponentInChildren<Text>().text != ""||
+            boxes[1].GetComponentInChildren<Text>().text == boxes[4].GetComponentInChildren<Text>().text && boxes[4].GetComponentInChildren<Text>().text == boxes[7].GetComponentInChildren<Text>().text && boxes[7].GetComponentInChildren<Text>().text != ""||
+            boxes[2].GetComponentInChildren<Text>().text == boxes[5].GetComponentInChildren<Text>().text && boxes[5].GetComponentInChildren<Text>().text == boxes[8].GetComponentInChildren<Text>().text && boxes[8].GetComponentInChildren<Text>().text != ""||
+            boxes[0].GetComponentInChildren<Text>().text == boxes[4].GetComponentInChildren<Text>().text && boxes[4].GetComponentInChildren<Text>().text == boxes[8].GetComponentInChildren<Text>().text && boxes[8].GetComponentInChildren<Text>().text != ""||
+            boxes[2].GetComponentInChildren<Text>().text == boxes[4].GetComponentInChildren<Text>().text && boxes[4].GetComponentInChildren<Text>().text == boxes[6].GetComponentInChildren<Text>().text && boxes[6].GetComponentInChildren<Text>().text != ""
+        )
+        {            
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+            
+    }
+}
---
+++ TicTacToe/Assets/Scripts/SPlay.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SPlay : MonoBehaviour
+{
+    private Text xo;
+    private bool validMove = true;
+
+    //Had to take a reference from this
+    public Image board;
+    //To access reference to GameRules script
+    private SGameRules sGameRulesRef;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get reference  to Text component to change it into X or O
+        xo = GetComponentInChildren<Text>();
+        //Get GameRulesRef via "board"
+        sGameRulesRef = board.GetComponent<SGameRules>();  
+    }
+   
+    //Needed to use EventTrigger T_T , no need for collider2D
+    public void OnClicked()
+    {
+        if (validMove && !sGameRulesRef.gameOver)
+        {
+            //Make a move and draw X or O
+            xo.text = SetValue();
+            sGameRulesRef.OneMovePlayed();
+            sGameRulesRef.CheckCondition();
+            //This grid can no longer be played
+            validMove = false;
+        }
+
+    }
+
+
+    public string SetValue()
+    {
+        if (sGameRulesRef.playerTurn)
+        {
+            return "O";
+        }
+        else
+        {
+            return "X";
+        }
+    }
+}
